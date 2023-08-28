a5b9864f440d4106b395b43061525a3e3e0d6b44
newte <pmichaudlauture@gmail.com>
2023-08-28 17:28:15 +0200

    update calendrier, maj pop up

--- src/app/calendrier/calendrier.component.ts
+++ src/app/calendrier/calendrier.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import {
   CalendarEvent,
   CalendarMonthViewBeforeRenderEvent,
   CalendarView,
 } from 'angular-calendar';
+import { isSameDay, isSameMonth } from 'date-fns';
 
 @Component({
   selector: 'app-calendrier',
@@ -11,21 +12,37 @@ import {
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./calendrier.component.css'],
 })
-export class CalendrierComponent implements OnInit {
+export class CalendrierComponent {
   viewDate: Date = new Date();
-
   view: CalendarView = CalendarView.Month;
-
+  CalendarView = CalendarView;
+  activeDayIsOpen = false;
   events: CalendarEvent[] = [
     {
-      title: 'Rentrée du BPC - Inscription',
+      title: 'Rentrée du BPC - Inscription - Auberge espagnole 20h00-01h00',
       start: new Date('2023-08-31T20:00'),
-      end: new Date('2023-08-31T1h00'),
+      end: new Date('2023-08-31T23:30'),
     },
   ];
   constructor() {}
 
-  ngOnInit(): void {}
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date;
+    }
+  }
+
+  closeOpenMonthViewDay() {
+    this.activeDayIsOpen = false;
+  }
 
   beforeMonthViewRender(renderEvent: CalendarMonthViewBeforeRenderEvent): void {
     renderEvent.body.forEach((day) => {
